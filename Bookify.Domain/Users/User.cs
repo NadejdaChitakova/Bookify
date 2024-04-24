4451abb8def552b8611a171820eecb6046c39877
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-04-24 14:17:19 +0300

    Implemented User domain entity

---
+++ Bookify.Domain/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.User;
+
+public class User : Entity
+{
+    public User(Guid id) : base(id)
+    {
+    }
+public FirstName FirstName { get; private set; }
+
+public LastName LastName { get; private set; }
+
+public Email Email { get; private set; }
+
+}
