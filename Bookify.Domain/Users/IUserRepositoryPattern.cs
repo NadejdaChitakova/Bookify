9565a03bd7e65ca0adae65464341b95403fac0f2
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-04-24 15:18:12 +0300

    Implemented repository pattern for apartment and user. Added unit of work pattern

---
+++ Bookify.Domain/Users/IUserRepositoryPattern.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.User;
+
+public interface IUserRepositoryPattern
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellation = default);
+
+    void Add(User user);
+}
