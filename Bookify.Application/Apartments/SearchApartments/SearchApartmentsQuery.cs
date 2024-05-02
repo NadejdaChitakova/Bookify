279187f1a7f69fab75af4728ae99afdbc413733f
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-02 21:21:38 +0300

    Implementing queries with Dapper for blazing perfomence.

---
+++ Bookify.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Apartments.SearchAppartments;
+
+public record SearchApartmentsQuery(
+    DateOnly dateStart,
+    DateOnly dateEnd): IQuery<IReadOnlyList<ApartmentResponse>>;
---
+++ Bookify.Application/Booking/GetBooking/BookingResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace Bookify.Application.Booking.GetBooking
+{
+    public sealed class BookingResponse
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid ApartmentId { get; set; }
+
+        public int Status { get; set; }
+
+        public decimal PriceAmount { get; set; }
+
+        public string PriceCurrency { get; set; }
+
+        public decimal CleaningFeeAmount { get; set; }
+
+        public decimal CleaningFeeCurrency { get; set; }  
+
+        public decimal AmenitiesUpChargeAmount { get; set; }
+
+        public decimal AmenitiesUpChargeCurrency { get; set; }
+
+        public decimal TotalPriceAmount { get; set; }
+
+        public decimal TotalPriceCurrency { get; set;}
+
+        public decimal DurationStart { get; set; }
+
+        public decimal DurationEnd { get; set; }
+
+        public decimal CreatedOnUtc { get; set; }
+    }
+}
