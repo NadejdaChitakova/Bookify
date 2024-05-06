3f27ba1aabae7e58f22e81cc6b8d4a0382109706
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-06 22:17:53 +0300

    Added sql script for get booking with dapper

--- Bookify.Application/Booking/GetBooking/GetBookingQueryHandle.cs
+++ Bookify.Application/Booking/GetBooking/GetBookingQueryHandle.cs
@@ -19,9 +19,24 @@ public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, Cance
             using var connection = _connectionFactory.CreateConnection();
 
 const string sql = """
-                   SELECT 
-                   id as Id,
-                   
+                   SELECT
+                       id AS Id,
+                       apartment_id AS ApartmentId,
+                       user_id AS UserId,
+                       status AS Status,
+                       price_for_period_amount AS PriceAmount,
+                       price_for_period_currency AS PriceCurrency,
+                       cleaning_fee_amount AS CleaningFeeAmount,
+                       cleaning_fee_currency AS CleaningFeeCurrency,
+                       amenities_up_charge_amount AS AmenitiesUpChargeAmount,
+                       amenities_up_charge_currency AS AmenitiesUpChargeCurrency,
+                       total_price_amount AS TotalPriceAmount,
+                       total_price_currency AS TotalPriceCurrency,
+                       duration_start AS DurationStart,
+                       duration_end AS DurationEnd,
+                       created_on_utc AS CreatedOnUtc
+                   FROM bookings
+                   WHERE id = @BookingId
                    """;
 
             var booking = await connection.QueryFirstOrDefaultAsync(
