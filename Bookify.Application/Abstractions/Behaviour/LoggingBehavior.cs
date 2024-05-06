22dffb276b8f65053fe1d33dfb032268bd29e9c1
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-06 20:54:23 +0300

    Implemented logging behaviour

---
+++ Bookify.Application/Abstractions/Behaviour/LoggingBehavior.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Behaviour
+{
+    public class LoggingBehavior<TRequest, TResponse>
+    :IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand
+    {
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
