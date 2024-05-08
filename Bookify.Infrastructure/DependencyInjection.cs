dfb595021b2ad72c2030c9ad6c97eb3a1cb12918
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-08 18:52:09 +0300

    Implementing repositories

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,12 @@
 ﻿using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Bookify.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +28,12 @@ namespace Bookify.Infrastructure
             {
                 options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
             });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IApartmentRepository, ApartmentRepository>();
+
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
             return services;
         }
---
+++ Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,6 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Infrastructure.Repositories
+{
+    internal sealed class UserRepository(ApplicationDbContext context) : Repository<User>(context), IUserRepository;
+}
