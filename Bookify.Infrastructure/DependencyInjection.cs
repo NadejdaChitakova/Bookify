ed4c0102d8094708c33ba9d403cc6a091d980fe2
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-08 17:21:35 +0300

    Create dependency injection for services. Added needed packages Added project reference to Application layer and created implementation to email and clock service. Register services at dependency injection

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+            services.AddTransient<IEmailService, EmailService>();
+
+            var connectionString = configuration.GetConnectionString("Database") ??
+                                   throw new ArgumentNullException(nameof(configuration));
+
+            return services;
+        }
+    }
+}
---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Infrastructure.Email
+{
+    public sealed class EmailService : IEmailService
+    {
+        public Task SendAsync(Domain.Users.Email recipient, string subject, string body)
+        {
+return Task.CompletedTask;
+        }
+    }
+}
