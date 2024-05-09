307b726d051f0877e92fca8306f9b7d496bbf1d2
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-09 20:20:12 +0300

    Created booking and apartment feature folder and two query methods to read from db

---
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Application.Apartments.SearchAppartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments
+{
+    [ApiController]
+    [Route("api/Apartments")]
+    public class ApartmentsController(ISender sender) : ControllerBase
+    {
+        private readonly ISender _sender = sender;
+
+        [HttpGet]
+        public async Task<ActionResult> SearchApartment(
+            DateOnly startDate,
+            DateOnly endDate,
+            CancellationToken cancellationToken)
+        {
+            var query = new SearchApartmentsQuery(startDate, endDate);
+
+            var result = await _sender.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Bookify.Api/Controllers/Bookings/BookingsController.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Application.Booking.GetBooking;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Bookings
+{
+    [ApiController]
+    [Route("api/Apartments")]
+    public class BookingsController(ISender sender) : ControllerBase
+    {
+        private readonly ISender _sender = sender;
+
+        [HttpGet]
+        public async Task<ActionResult> GetBooking(Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetBookingQuery(id);
+
+            var result = await _sender.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
