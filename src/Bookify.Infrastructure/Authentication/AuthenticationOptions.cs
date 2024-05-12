2eeb5c64faf67037b0e37e4dead85e4cfaabe5a9
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-12 19:06:16 +0300

    Added class authentication options

---
+++ src/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookify.Infrastructure.Authentication
+{
+    public sealed class AuthenticationOptions
+    {
+        public string Audience { get; init; } = string.Empty;
+
+        public string MetadataUrl { get; init; } = string.Empty;
+
+        public bool RequireHttpMetadata { get; init; }
+
+        public string Issuer { get; init; } = string.Empty;
+    }
+}
