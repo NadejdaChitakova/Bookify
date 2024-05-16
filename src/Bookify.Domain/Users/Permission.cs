6ea874ec6c7e9ba2ce97f9c0d266eb9f066be0fd
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-16 20:02:40 +0300

    Created permission, rolePermission entities

---
+++ src/Bookify.Domain/Users/Permission.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Domain.Users
+{
+    public sealed class Permission(
+        int id, 
+        string name)
+    {
+        public static readonly Permission UserRead = new Permission(1, "user:read");
+
+        public int Id { get; init; }
+        public string Name { get; init; }
+    }
+}
