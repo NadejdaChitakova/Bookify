1f513f02404ea94c0724b619a58b54591bde5bc3
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-19 14:00:07 +0300

    Implemented db configuration for apartment image entity and applied migration to update db

---
+++ src/Bookify.Infrastructure/Configurations/ApartmentImageConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.AttachedFiles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations
+{
+    internal sealed class ApartmentImageConfiguration : IEntityTypeConfiguration<ApartmentImage>
+    {
+        public void Configure(EntityTypeBuilder<ApartmentImage> builder)
+        {
+            builder.ToTable("apartment_image");
+
+            builder.HasKey(image => image.Id);
+
+            builder.Property(apartment => apartment.FileContent)
+                .HasMaxLength(2000)
+                .HasConversion(name => name.Value, value => new FileContent(value));
+
+            builder.Property(apartment => apartment.Extension)
+                .HasMaxLength(2000)
+                .HasConversion(name => name.Value, value => new Extension(value));
+
+            builder.HasOne<Apartment>()
+                .WithMany()
+                .HasForeignKey(review => review.ApartmentId);
+
+        }
+    }
+}
