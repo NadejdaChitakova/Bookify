d8b20a04c77e079572c05ea1a67fdb7143a98ecb
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-05-19 12:41:30 +0300

    Created apartment service

---
+++ src-fe/Bookify-frontend/src/services/app.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Options } from '../types/options';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(
+    private httpClient: HttpClient) { }
+
+    get<T>(url: string, options: Options): Observable<T>{
+      return this.httpClient.get<T>(url, options) as Observable<T>;
+    }
+}
