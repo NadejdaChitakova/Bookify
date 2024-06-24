152042fef96b9d779e3a61143027c547a6eed21d
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-06-24 14:57:00 +0300

    Implemented user related unit tests

--- Bookify.Domain.UnitTests/Users/UserTests.cs
+++ Bookify.Domain.UnitTests/Users/UserTests.cs
@@ -1,8 +1,10 @@
-﻿using FluentAssertions;
+﻿using Bookify.Domain.UnitTests.Infrastructure;
+using Bookify.Domain.User.Events;
+using FluentAssertions;
 
 namespace Bookify.Domain.UnitTests.Users
 {
-    public class UserTests
+    public class UserTests : BaseTest
     {
         [Fact]
         public void Create_Should_SetPropertyValues()
@@ -14,6 +16,28 @@ namespace Bookify.Domain.UnitTests.Users
             user.FirstName.Should().Be(UserData.FirstName);
             user.LastName.Should().Be(UserData.LastName);
             user.Email.Should().Be(UserData.Email);
+        }
+
+        [Fact]
+        public void Create_Should_RaiseUserCreatedDomainEvent()
+        {
+            //Act
+            var user = Domain.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+            //Assert
+            var domainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+            domainEvent.UserId.Should().Be(user.Id);
+        }
+
+        [Fact]
+        public void Create_Should_AddRegistedRoleToUser()
+        {
+            //Act
+            var user = Domain.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+            //Assert
+            user.Roles.Should().Contain(Domain.Users.Role.Registered);
         }
     }
 }
