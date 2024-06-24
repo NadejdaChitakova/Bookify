342f80c4ecac3db7f933ddf5c680ed3360473244
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-06-24 14:44:27 +0300

    Configure packages and added first test

---
+++ Bookify.Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace Bookify.Domain.UnitTests.Users
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Create_Should_SetPropertyValues()
+        {
+            //Act
+            var user = Domain.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+            //Assert
+            user.FirstName.Should().Be(UserData.FirstName);
+            user.LastName.Should().Be(UserData.LastName);
+            user.Email.Should().Be(UserData.Email);
+        }
+    }
+}
